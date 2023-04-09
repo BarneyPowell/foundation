bc4ce6029c55d37224076ec784f725fadca422ac
Barney Powell <barney.powell@charlottetilbury.com>
2023-04-09 13:23:57 +0100

    Add bundle analyzer package to nextjs app

--- apps/www-nextjs/next.config.js
+++ apps/www-nextjs/next.config.js
@@ -1,3 +1,9 @@
+const withPlugins = require('next-compose-plugins');
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+  openAnalyzer: false
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true, // Recommended for the `pages` directory, default in `app`.
@@ -7,4 +13,8 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+module.exports = withPlugins([
+  [withBundleAnalyzer],
+  // your other plugins here
+], nextConfig);
+
