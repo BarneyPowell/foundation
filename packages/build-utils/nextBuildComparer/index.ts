7d49f8b103dca10828307581474f13bc9f9a4580
Barney Powell <barney.powell@charlottetilbury.com>
2023-04-12 09:41:16 +0100

    Create build utils project and update build script to store output

---
+++ packages/build-utils/nextBuildComparer/index.ts
@@ -0,0 +1,92 @@
+import { readFile } from "fs/promises";
+import { NextBuildComparerOptions } from './types';
+
+export type {
+  NextBuildComparerOptions,
+} from './types';
+
+export type BuildAsset = {
+  label: string;
+  statSize: number;
+  parsedSize: number;
+  gzipSize: number;
+  isInitialByEntrypoint: { [index: string]: boolean }
+}
+
+export type Entrypoint = {
+  name: string;
+  totalStatSize: number;
+  totalParsedSize: number;
+  totalGzipSize: number;
+  assets: BuildAsset[];
+};
+
+export type EntrypointsByKey = {
+  [index: string]: Entrypoint
+}
+
+async function loadBuildAssets(path: string) {
+  const file = await readFile(path, "utf8");
+  return JSON.parse(file) as BuildAsset[];
+}
+
+const calculateEntrypointSizes = (bundles: BuildAsset[]): EntrypointsByKey => {
+  const entrypoints: EntrypointsByKey = {};
+
+  for(let x = 0; x < bundles.length; x++) {
+    let { isInitialByEntrypoint, statSize, parsedSize, gzipSize } = bundles[x];
+
+    let entrypointKeys = Object.keys(isInitialByEntrypoint);
+    entrypointKeys.forEach((key: string) => {
+      if (!isInitialByEntrypoint[key]) return;
+      if (!entrypoints[key]) {
+        entrypoints[key] = {
+          name: key,
+          assets: [],
+          totalGzipSize: 0,
+          totalParsedSize: 0,
+          totalStatSize: 0
+        };
+      }
+
+      let entrypoint = entrypoints[key];
+      entrypoint.totalGzipSize += gzipSize;
+      entrypoint.totalStatSize += statSize;
+      entrypoint.totalParsedSize += parsedSize;
+      entrypoint.assets.push(bundles[x]);
+      entrypoints[key] = entrypoint;
+    })
+
+  }
+
+  return entrypoints;
+    
+}
+
+const generateTable = (entrypoints: EntrypointsByKey): string => {
+  let output = '';
+
+  // header row
+  output += `| Entrypoint | Initial gzipped total |`;
+  output += `| --- | --- |`;
+
+  Object.keys(entrypoints).forEach((key) => {
+    const entrypoint = entrypoints[key];
+
+    output += `| ${entrypoint.name} | ${entrypoint.totalGzipSize} |`;
+
+  });
+
+  return output;
+}
+
+const nextBuildComparer = async (options: NextBuildComparerOptions) => {
+  const { currentBuildJsonPath } = options;
+  const currentBundles = await loadBuildAssets(currentBuildJsonPath);
+  const entrypoints = calculateEntrypointSizes(currentBundles);
+  return generateTable(entrypoints);
+};
+
+export {
+  nextBuildComparer
+};
---
+++ packages/build-utils/nextBundleAnalyzer/index.ts
@@ -0,0 +1,52 @@
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
+
+// import type { Options } from './plugin/interfaces/Options';
+// import { getInternalOptions } from './plugin/utils/getInternalOptions';
+
+// export type {
+//   BuildStats,
+//   ChunkGroup,
+//   CommonChunk,
+//   Page,
+// } from './interfaces/BuildStats';
+
+export type NextBundleAnalyzerOptions = BundleAnalyzerPlugin.Options & {
+  enabled: boolean,
+  openAnalyzer: boolean
+};
+
+export function withNextBundleAnalyzer(options: NextBundleAnalyzerOptions) {
+  // const internalOptions = getInternalOptions(options);
+  // const { clientOnly, enabled, reportDir } = internalOptions;
+  // let { reportFilename } = internalOptions;
+
+  return (nextConfig: any = {}) => ({
+    ...nextConfig,
+    webpack(webpackConfig: any, webpackOptions: any) {
+      // const { isServer } = webpackOptions;
+      if (options.enabled) {
+
+        const filenameExt = options.analyzerMode === 'json'
+          ? '.json'
+          : '.html';
+
+        const analyzerMode = 'static';
+        const reportFilename = !webpackOptions.nextRuntime
+          ? `./analyze/client${filenameExt}`
+          : `../${webpackOptions.nextRuntime === 'nodejs' ? '../' : ''}analyze/${webpackOptions.nextRuntime}${filenameExt}`;
+
+        webpackConfig.plugins.push(
+          new BundleAnalyzerPlugin({
+            analyzerMode,
+            reportFilename,
+            ...options
+          })
+        );
+      }
+
+      return typeof nextConfig.webpack === 'function'
+        ? nextConfig.webpack(webpackConfig, webpackOptions)
+        : webpackConfig;
+    },
+  });
+}
